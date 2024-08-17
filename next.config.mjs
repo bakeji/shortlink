448f83327f0eb6394ca450401857d15e7904d967
Fatimoh Bakare <fatimohbakare3@gmail.com>
2024-08-17 22:46:03 +0100

    fixed api issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://urlbae.com/api/:path*'
-          }
-        ]
-      },
-
+ 
       images: {
         remotePatterns: [
           {
